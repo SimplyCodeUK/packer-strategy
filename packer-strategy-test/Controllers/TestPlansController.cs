9379d8edb743bf8851a4e39edfab5138f1f5ed6b
Louis Henry Nayegon <louis_nayegon@hotmail.com>
2017-07-31 19:36:09 +0100

    Add http patch tests

--- packer-strategy-test/Controllers/TestPlansController.cs
+++ packer-strategy-test/Controllers/TestPlansController.cs
@@ -7,6 +7,7 @@ using System;
 using System.Collections.Generic;
 using System.Net;
 
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using NUnit.Framework;
@@ -120,16 +121,27 @@ namespace packer_strategy_test
         [Test]
         public void Get()
         {
+            string          startName = "A name";
+            string          startNote = "Some notes";
             PlansController controller = new PlansController(planRepository);
             string          id = Guid.NewGuid().ToString();
+            Plan            plan = new Plan { Id = id, Name = startName, Notes = startNote };
 
-            controller.Post(new Plan { Id = id });
+            controller.Post(plan);
 
             var result = controller.Get(id);
 
             Assert.IsNotNull(result);
             Assert.IsInstanceOf<OkObjectResult>(result);
-            Assert.AreEqual((int)HttpStatusCode.OK, ((OkObjectResult)result).StatusCode);
+
+            OkObjectResult objectResult = (OkObjectResult)result;
+            Assert.AreEqual((int)HttpStatusCode.OK, objectResult.StatusCode);
+            Assert.IsInstanceOf<Plan>(objectResult.Value);
+
+            plan = (Plan)objectResult.Value;
+            Assert.AreEqual(plan.Id, id);
+            Assert.AreEqual(plan.Name, startName);
+            Assert.AreEqual(plan.Notes, startNote);
         }
 
         /** (Unit Test Method) gets not found. */
@@ -138,8 +150,7 @@ namespace packer_strategy_test
         {
             PlansController controller = new PlansController(planRepository);
             string          id = Guid.NewGuid().ToString();
-
-            var result = controller.Get(id);
+            var             result = controller.Get(id);
 
             Assert.IsNotNull(result);
             Assert.IsInstanceOf<NotFoundResult>(result);
@@ -150,18 +161,31 @@ namespace packer_strategy_test
         [Test]
         public void Put()
         {
+            string          startName = "A name";
+            string          putName = "B name";
             PlansController controller = new PlansController(planRepository);
             string          id = Guid.NewGuid().ToString();
-            Plan            plan = new Plan { Id = id, Name = "A name" };
+            Plan            plan = new Plan { Id = id, Name = startName };
 
             controller.Post(plan);
 
-            plan.Name = "B name";
+            plan.Name = putName;
             var result = controller.Put(id, plan);
 
             Assert.IsNotNull(result);
             Assert.IsInstanceOf<OkResult>(result);
             Assert.AreEqual((int)HttpStatusCode.OK, ((OkResult)result).StatusCode);
+
+            // Get the plan and check the returned object has the new Name
+            result = controller.Get(id);
+            Assert.IsInstanceOf<OkObjectResult>(result);
+
+            OkObjectResult objectResult = (OkObjectResult)result;
+            Assert.AreEqual((int)HttpStatusCode.OK, objectResult.StatusCode);
+            Assert.IsInstanceOf<Plan>(objectResult.Value);
+            plan = (Plan)objectResult.Value;
+            Assert.AreEqual(plan.Id, id);
+            Assert.AreEqual(plan.Name, putName);
         }
 
         /** (Unit Test Method) puts not found. */
@@ -169,10 +193,9 @@ namespace packer_strategy_test
         public void PutNotFound()
         {
             PlansController controller = new PlansController(planRepository);
-            string id = Guid.NewGuid().ToString();
-            Plan plan = new Plan();
-
-            var result = controller.Put(id, plan);
+            string          id = Guid.NewGuid().ToString();
+            Plan            plan = new Plan();
+            var             result = controller.Put(id, plan);
 
             Assert.IsNotNull(result);
             Assert.IsInstanceOf<NotFoundResult>(result);
@@ -184,8 +207,8 @@ namespace packer_strategy_test
         public void Delete()
         {
             PlansController controller = new PlansController(planRepository);
-            string id = Guid.NewGuid().ToString();
-            Plan plan = new Plan { Id = id };
+            string          id = Guid.NewGuid().ToString();
+            Plan            plan = new Plan { Id = id };
 
             controller.Post(plan);
 
@@ -201,9 +224,76 @@ namespace packer_strategy_test
         public void DeleteNotFound()
         {
             PlansController controller = new PlansController(planRepository);
-            string id = Guid.NewGuid().ToString();
+            string          id = Guid.NewGuid().ToString();
+            var             result = controller.Delete(id);
 
-            var result = controller.Delete(id);
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOf<NotFoundResult>(result);
+            Assert.AreEqual((int)HttpStatusCode.NotFound, ((NotFoundResult)result).StatusCode);
+        }
+
+        /** (Unit Test Method) patches this object. */
+        [Test]
+        public void Patch()
+        {
+            string          startName = "A name";
+            string          patchName = "B name";
+            string          startNote = "Some notes";
+            PlansController controller = new PlansController(planRepository);
+            string          id = Guid.NewGuid().ToString();
+            Plan            plan = new Plan { Id = id, Name = startName, Notes = startNote };
+
+            // Create a new plan
+            controller.Post(plan);
+
+            // Patch the plan with a new name
+            JsonPatchDocument<Plan> patch = new JsonPatchDocument<Plan>();
+            patch.Replace(e => e.Name, patchName);
+
+            var result = controller.Patch(id, patch);
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOf<OkObjectResult>(result);
+
+            // Check the returned object from the patch has the same Note but different Name
+            OkObjectResult objectResult = (OkObjectResult)result;
+            Assert.AreEqual((int)HttpStatusCode.OK, objectResult.StatusCode);
+            Assert.IsInstanceOf<Plan>(objectResult.Value);
+
+            plan = (Plan)objectResult.Value;
+            Assert.AreEqual(plan.Id, id);
+            Assert.AreEqual(plan.Name, patchName);
+            Assert.AreEqual(plan.Notes, startNote);
+
+            // Get the plan and check the returned object has the same Note and new Name
+            result = controller.Get(id);
+            Assert.IsInstanceOf<OkObjectResult>(result);
+            objectResult = (OkObjectResult)result;
+            Assert.AreEqual((int)HttpStatusCode.OK, objectResult.StatusCode);
+            Assert.IsInstanceOf<Plan>(objectResult.Value);
+
+            plan = (Plan)objectResult.Value;
+            Assert.AreEqual(plan.Id, id);
+            Assert.AreEqual(plan.Name, patchName);
+            Assert.AreEqual(plan.Notes, startNote);
+        }
+
+        /** (Unit Test Method) patches this object. */
+        [Test]
+        public void PatchNotFound()
+        {
+            string          startName = "A name";
+            string          patchName = "B name";
+            string          startNote = "Some notes";
+            PlansController controller = new PlansController(planRepository);
+            Plan            plan = new Plan { Id = Guid.NewGuid().ToString(), Name = startName, Notes = startNote };
+
+            controller.Post(plan);
+
+            JsonPatchDocument<Plan> patch = new JsonPatchDocument<Plan>();
+            patch.Replace(e => e.Name, patchName);
+
+            var result = controller.Patch(Guid.NewGuid().ToString(), patch);
 
             Assert.IsNotNull(result);
             Assert.IsInstanceOf<NotFoundResult>(result);
