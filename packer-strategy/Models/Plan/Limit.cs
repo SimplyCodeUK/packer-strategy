42c45065223ccfcadb54ec08e79c761dabf822af
Louis Henry Nayegon <louis_nayegon@hotmail.com>
2017-08-04 11:36:01 +0100

    Start plan limit implementation

---
+++ packer-strategy/Models/Plan/Limit.cs
@@ -0,0 +1,428 @@
+﻿//
+// Copyright (c) Simply Code Ltd. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE file in the project root for full license information.
+//
+
+namespace packer_strategy.Models.Plan
+{
+    /// <summary>
+    ///     A limit.
+    /// </summary>
+    public class Limit
+    {
+        /// <summary>
+        ///     Gets or sets the identifier of the plan.
+        /// </summary>
+        ///
+        /// <value>
+        ///     The identifier of the plan.
+        /// </value>
+        public string PlanId { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the level.
+        /// </summary>
+        ///
+        /// <value>
+        ///     The level.
+        /// </value>
+        public Stage.Levels Level { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the zero-based index of this object.
+        /// </summary>
+        ///
+        /// <value>
+        ///     The index.
+        /// </value>
+        public long Index { get; set; }
+
+        /// <summary>
+        ///     Gets or sets a value indicating whether the design.
+        /// </summary>
+        ///
+        /// <value>
+        ///     True if design, false if not.
+        /// </value>
+        public bool Design { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the type of the material.
+        /// </summary>
+        ///
+        /// <value>
+        ///     The type of the material.
+        /// </value>
+        public Material.Material.Type MaterialType { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the material code.
+        /// </summary>
+        ///
+        /// <value>
+        ///     The material code.
+        /// </value>
+        public string MaterialCode { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the material code minimum.
+        /// </summary>
+        ///
+        /// <value>
+        ///     The material code minimum.
+        /// </value>
+        public string MaterialCodeMin { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the type of the design.
+        /// </summary>
+        ///
+        /// <value>
+        ///     The type of the design.
+        /// </value>
+        public string DesignType { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the design code.
+        /// </summary>
+        ///
+        /// <value>
+        ///     The design code.
+        /// </value>
+        public string DesignCode { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the design code minimum.
+        /// </summary>
+        ///
+        /// <value>
+        ///     The design code minimum.
+        /// </value>
+        public string DesignCodeMin { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the type of the quality.
+        /// </summary>
+        ///
+        /// <value>
+        ///     The type of the quality.
+        /// </value>
+        public int QualityType { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the quality code.
+        /// </summary>
+        ///
+        /// <value>
+        ///     The quality code.
+        /// </value>
+        public string QualityCode { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the quality code minimum.
+        /// </summary>
+        ///
+        /// <value>
+        ///     The quality code minimum.
+        /// </value>
+        public string QualityCodeMin { get; set; }
+
+        /// <summary>
+        ///     Gets or sets a value indicating whether the inverted.
+        /// </summary>
+        ///
+        /// <value>
+        ///     True if inverted, false if not.
+        /// </value>
+        public bool Inverted { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the number of layers.
+        /// </summary>
+        ///
+        /// <value>
+        ///     The number of layers.
+        /// </value>
+        public long LayerCount { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the layer start.
+        /// </summary>
+        ///
+        /// <value>
+        ///     The layer start.
+        /// </value>
+        public long LayerStart { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the layer step.
+        /// </summary>
+        ///
+        /// <value>
+        ///     The layer step.
+        /// </value>
+        public long LayerStep { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the quality caliper.
+        /// </summary>
+        ///
+        /// <value>
+        ///     The quality caliper.
+        /// </value>
+        public double QualityCaliper { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the quality density.
+        /// </summary>
+        ///
+        /// <value>
+        ///     The quality density.
+        /// </value>
+        public double QualityDensity { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the length minimum.
+        /// </summary>
+        ///
+        /// <value>
+        ///     The length minimum.
+        /// </value>
+        public double LengthMin { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the length maximum.
+        /// </summary>
+        ///
+        /// <value>
+        ///     The length maximum.
+        /// </value>
+        public double LengthMax { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the breadth minimum.
+        /// </summary>
+        ///
+        /// <value>
+        ///     The breadth minimum.
+        /// </value>
+        public double BreadthMin { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the breadth maximum.
+        /// </summary>
+        ///
+        /// <value>
+        ///     The breadth maximum.
+        /// </value>
+        public double BreadthMax { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the height minimum.
+        /// </summary>
+        ///
+        /// <value>
+        ///     The height minimum.
+        /// </value>
+        public double HeightMin { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the height maximum.
+        /// </summary>
+        ///
+        /// <value>
+        ///     The height maximum.
+        /// </value>
+        public double HeightMax { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the caliper minimum.
+        /// </summary>
+        ///
+        /// <value>
+        ///     The caliper minimum.
+        /// </value>
+        public double CaliperMin { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the caliper maximum.
+        /// </summary>
+        ///
+        /// <value>
+        ///     The caliper maximum.
+        /// </value>
+        public double CaliperMax { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the length of the packing gap.
+        /// </summary>
+        ///
+        /// <value>
+        ///     The length of the packing gap.
+        /// </value>
+        public double PackingGapLength { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the packing gap breadth.
+        /// </summary>
+        ///
+        /// <value>
+        ///     The packing gap breadth.
+        /// </value>
+        public double PackingGapBreadth { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the height of the packing gap.
+        /// </summary>
+        ///
+        /// <value>
+        ///     The height of the packing gap.
+        /// </value>
+        public double PackingGapHeight { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the safety factor minimum.
+        /// </summary>
+        ///
+        /// <value>
+        ///     The safety factor minimum.
+        /// </value>
+        public double SafetyFactorMin { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the safety factor maximum.
+        /// </summary>
+        ///
+        /// <value>
+        ///     The safety factor maximum.
+        /// </value>
+        public double SafetyFactorMax { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the front placement.
+        /// </summary>
+        ///
+        /// <value>
+        ///     The front placement.
+        /// </value>
+        public long FrontPlacement { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the back placement.
+        /// </summary>
+        ///
+        /// <value>
+        ///     The back placement.
+        /// </value>
+        public long BackPlacement { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the left placement.
+        /// </summary>
+        ///
+        /// <value>
+        ///     The left placement.
+        /// </value>
+        public long LeftPlacement { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the right placement.
+        /// </summary>
+        ///
+        /// <value>
+        ///     The right placement.
+        /// </value>
+        public long RightPlacement { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the top placement.
+        /// </summary>
+        ///
+        /// <value>
+        ///     The top placement.
+        /// </value>
+        public long TopPlacement { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the bottom placement.
+        /// </summary>
+        ///
+        /// <value>
+        ///     The bottom placement.
+        /// </value>
+        public long BottomPlacement { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the length thicknesses.
+        /// </summary>
+        ///
+        /// <value>
+        ///     The length thicknesses.
+        /// </value>
+        public long LengthThicknesses { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the length sink change.
+        /// </summary>
+        ///
+        /// <value>
+        ///     The length sink change.
+        /// </value>
+        public long LengthSinkChange { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the breadth thicknesses.
+        /// </summary>
+        ///
+        /// <value>
+        ///     The breadth thicknesses.
+        /// </value>
+        public long BreadthThicknesses { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the breadth sink change.
+        /// </summary>
+        ///
+        /// <value>
+        ///     The breadth sink change.
+        /// </value>
+        public long BreadthSinkChange { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the height thicknesses.
+        /// </summary>
+        ///
+        /// <value>
+        ///     The height thicknesses.
+        /// </value>
+        public long HeightThicknesses { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the height sink change.
+        /// </summary>
+        ///
+        /// <value>
+        ///     The height sink change.
+        /// </value>
+        public long HeightSinkChange { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the type of the sl costing.
+        /// </summary>
+        ///
+        /// <value>
+        ///     The type of the sl costing.
+        /// </value>
+        public long SlCostingType { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the usage.
+        /// </summary>
+        ///
+        /// <value>
+        ///     The usage.
+        /// </value>
+        public long Usage { get; set; }
+    }
+}
