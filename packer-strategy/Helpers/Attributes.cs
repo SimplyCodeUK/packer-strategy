1b385de837642fe3ceda8153c609e8d23ad1f189
Louis Henry Nayegon <louis_nayegon@hotmail.com>
2017-08-14 21:29:06 +0100

    Add copyright header

--- packer-strategy/Helpers/Attributes.cs
+++ packer-strategy/Helpers/Attributes.cs
@@ -1,4 +1,10 @@
-﻿namespace packer_strategy.Helpers
+﻿//
+// Copyright (c) Simply Code Ltd. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE file in the project root for full license information.
+//
+
+namespace packer_strategy.Helpers
 {
     using System;
     using System.Collections.Generic;
