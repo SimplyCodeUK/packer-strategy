e30d9b74c7148338d4a3992f2afa17d2c148b7e7
Louis Henry Nayegon <louis.henry.nayegon@gmail.com>
2017-09-12 17:44:31 +0100

    Add MVC framework for UI

---
+++ PackItUI/Models/ErrorViewModel.cs
@@ -0,0 +1,24 @@
+// <copyright company="Simply Code Ltd.">
+// Copyright (c) Simply Code Ltd. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace PackItUI.Models
+{
+    using System;
+
+    /// <summary> Exception model. </summary>
+    public class ErrorViewModel
+    {
+        /// <summary>   Gets or sets the requested Id. </summary>
+        ///
+        /// <value> The position x coordinate. </value>
+        public string RequestId { get; set; }
+
+        /// <summary> Gets a flag indicating if the request id can be shown</summary>
+        /// 
+        /// <returns> A flag indicating if the request id can be shown.</returns>
+        public bool ShowRequestId => !string.IsNullOrEmpty(this.RequestId);
+    }
+}
---
+++ PackItUI/Program.cs
@@ -0,0 +1,40 @@
+﻿// <copyright company="Simply Code Ltd.">
+// Copyright (c) Simply Code Ltd. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace PackItUI
+{
+    using Microsoft.AspNetCore;
+    using Microsoft.AspNetCore.Hosting;
+
+    /// <summary>   A program. </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Prevents a default instance of the <see cref="Program" /> class from being created.
+        /// </summary>
+        private Program()
+        {
+        }
+
+        /// <summary>   Main entry-point for this application. </summary>
+        ///
+        /// <param name="args"> An array of command-line argument strings. </param>
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        /// <summary> Builds the web host. </summary>
+        /// 
+        /// <param name="args">The arguments.</param>
+        /// 
+        /// <returns> The web host. </returns>
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+    }
+}
