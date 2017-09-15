6fc15732ccd8744014d7f0216100b57afed5006e
Louis Henry Nayegon <louis.henry.nayegon@gmail.com>
2017-09-15 15:56:16 +0100

    Add about and use tables in UI

--- PackItUI/Models/MaterialsViewModel.cs
+++ PackItUI/Models/MaterialsViewModel.cs
@@ -7,16 +7,7 @@
 namespace PackItUI.Models
 {
     /// <summary> Materials model. </summary>
-    public class MaterialsViewModel
+    public class MaterialsViewModel : ServiceViewModel
     {
-        /// <summary>   Gets or sets the version of the service. </summary>
-        ///
-        /// <value> The verison of the service. </value>
-        public string Version { get; set; }
-
-        /// <summary> Gets or sets about information on the service. </summary>
-        /// 
-        /// <returns> About the service. </returns>
-        public string About { get; set; }
     }
 }
