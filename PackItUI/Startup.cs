485377a2098416171ae8e9213414484b70d9445b
Louis Henry Nayegon <louis.henry.nayegon@gmail.com>
2017-09-19 15:53:03 +0100

    Tidy up

--- PackItUI/Startup.cs
+++ PackItUI/Startup.cs
@@ -71,8 +71,8 @@ namespace PackItUI
                     name: "areaRoute",
                     template: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
                 routes.MapRoute(
-                         name: "default",
-                         template: "{controller=Home}/{action=Index}/{id?}");
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
