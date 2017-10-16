0e5299d87b6f5ceed887154f93c7692613a8988d
Louis Henry Nayegon <louis_nayegon@hotmail.com>
2017-10-16 13:09:48 +0100

    Add unit tests for PackItLib with rename of PackItTest to PackIt.Test
    Unit tests for PackItLib, PackItLib.Test, added.
    Rename of PackItTest to PackIt.Test
    Config file for appveyor added

--- PackItLib/src/PackItLib/Helpers/Attributes.cs
+++ PackItLib/src/PackItLib/Helpers/Attributes.cs
@@ -37,6 +37,30 @@ namespace PackIt.Helpers
             return displayAttribute?.ShortName ?? value.ToString();
         }
 
+        /// <summary> An Enum extension method that returns the name. </summary>
+        ///
+        /// <param name="value"> The value to act on. </param>
+        ///
+        /// <returns> The name. </returns>
+        public static string Name(this Enum value)
+        {
+            // get attributes  
+            FieldInfo field = value.GetType().GetField(value.ToString());
+            object[] attributes = field.GetCustomAttributes(false);
+
+            // Description is in a hidden Attribute class called DisplayAttribute
+            // Not to be confused with DisplayNameAttribute
+            dynamic displayAttribute = null;
+
+            if (attributes.Any())
+            {
+                displayAttribute = attributes.ElementAt(0);
+            }
+
+            // return description
+            return displayAttribute?.Name ?? value.ToString();
+        }
+
         /// <summary> An Enum extension method that returns the URL name. </summary>
         ///
         /// <param name="value"> The value to act on. </param>
---
+++ PackItLib/test/PackItLib.Test/Helpers/TestAttributes.cs
@@ -0,0 +1,71 @@
+﻿// <copyright company="Simply Code Ltd.">
+// Copyright (c) Simply Code Ltd. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace PackItLib.Test
+{
+    using NUnit.Framework;
+    using PackIt.Helpers;
+    using PackIt.Helpers.Enums;
+
+    /// <summary> (Unit Test Method) Enum attributes helpers. </summary>
+    [TestFixture]
+    public class TestAttributes
+    {
+        /// <summary> (Unit Test Method) get an enum short name. </summary>
+        [Test]
+        public void ShortName()
+        {
+            var name = Attributes.ShortName(MaterialType.UBoard);
+
+            Assert.AreEqual("UBoard", name);
+        }
+
+        /// <summary> (Unit Test Method) get an enum name. </summary>
+        [Test]
+        public void Name()
+        {
+            var name = Attributes.Name(MaterialType.UBoard);
+
+            Assert.AreEqual("U Board", name);
+        }
+
+        /// <summary> (Unit Test Method) get an enum url name. </summary>
+        [Test]
+        public void UrlName()
+        {
+            var name = Attributes.UrlName(MaterialType.UBoard);
+
+            Assert.AreEqual("uboard", name);
+        }
+
+        /// <summary> (Unit Test Method) get an enum short name with no attribute. </summary>
+        [Test]
+        public void ShortNameNotSpecified()
+        {
+            var name = Attributes.ShortName(CostType.Area);
+
+            Assert.AreEqual("Area", name);
+        }
+
+        /// <summary> (Unit Test Method) get an enum name with no attribute. </summary>
+        [Test]
+        public void NameNotSpecified()
+        {
+            var name = Attributes.Name(CostType.Area);
+
+            Assert.AreEqual("Area", name);
+        }
+
+        /// <summary> (Unit Test Method) get an enum url name with no attribute. </summary>
+        [Test]
+        public void UrlNameNotSpecified()
+        {
+            var name = Attributes.UrlName(CostType.Area);
+
+            Assert.AreEqual("area", name);
+        }
+    }
+}
