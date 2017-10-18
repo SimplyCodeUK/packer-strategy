c5d2c0cac96830f8df65cb5afd7652edf5e7ecfd
Louis Henry Nayegon <louis_nayegon@hotmail.com>
2017-10-18 16:17:19 +0100

    Move button out of tab panel

---
+++ PackItLib/src/PackItLib/Helpers/MaterialType/Factory.cs
@@ -0,0 +1,38 @@
+﻿// <copyright company="Simply Code Ltd.">
+// Copyright (c) Simply Code Ltd. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace PackIt.Helpers.MaterialType
+{
+    using System;
+    using System.Collections.Generic;
+
+    /// <summary> Material Type factory class. </summary>
+    public static partial class Factory
+    {
+        /// <summary>
+        ///  Dictionary of Capabilities
+        /// </summary>
+        private static Dictionary<Enums.MaterialType, Capabilities> dictionary = new Dictionary<Enums.MaterialType, Capabilities>()
+        {
+            { Enums.MaterialType.Pallet, new Pallet() }
+        };
+
+        /// <summary> Create Material Type factory function. </summary>
+        ///
+        /// <param name="type"> The Material Type enum. </param>
+        ///
+        /// <returns> The Material Type capabilities. </returns>
+        public static Capabilities Create(Enums.MaterialType type)
+        {
+            if (dictionary.ContainsKey(type))
+            {
+                return dictionary[type];
+            }
+
+            return new Default();
+        }
+    }
+}
