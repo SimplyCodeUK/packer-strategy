684a684f4f0ca9ab9516d5a344c15b46dee02d94
Louis Henry Nayegon <louis.henry.nayegon@gmail.com>
2017-10-18 16:17:19 +0100

    Move button out of tab panel

---
+++ PackItLib/src/PackItLib/Helpers/MaterialType/Pallet.cs
@@ -0,0 +1,23 @@
+﻿// <copyright company="Simply Code Ltd.">
+// Copyright (c) Simply Code Ltd. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace PackIt.Helpers.MaterialType
+{
+    /// <summary> Material Type factory class. </summary>
+    public static partial class Factory
+    {
+        /// <summary>   Values that represent material type Pallet. </summary>
+        private class Pallet : Capabilities
+        {
+            /// <summary>
+            /// Initialises a new instance of the <see cref="Pallet"/> class.
+            /// </summary>
+            public Pallet() : base(hasPalletDecks: true)
+            {
+            }
+        }
+    }
+}
