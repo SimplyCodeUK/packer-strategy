e76e443ef51877d6bbbdb5b4c2a5bfcb21929281
Louis Henry Nayegon <louis.henry.nayegon@gmail.com>
2017-11-06 18:52:01 +0000

    Add a getter for timeout

--- PackItUI/src/PackItUI/Areas/Packs/DTO/PackHandler.cs
+++ PackItUI/src/PackItUI/Areas/Packs/DTO/PackHandler.cs
@@ -48,11 +48,16 @@ namespace PackItUI.Areas.Packs.DTO
             this.endpoint = appSettings.Value.ServiceEndpoints.Packs;
         }
 
-        /// <summary> Sets the time out for http calls. </summary>
+        /// <summary> Gets or sets the time out for http calls. </summary>
         ///
         /// <value> The time out. </value>
         public TimeSpan TimeOut
         {
+            get
+            {
+                return this.httpClient.Timeout;
+            }
+
             set
             {
                 this.httpClient.Timeout = value;
