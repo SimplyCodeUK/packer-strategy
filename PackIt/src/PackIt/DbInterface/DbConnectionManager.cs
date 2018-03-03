2d1098025bd4d5aae29e8357c2da534add7ab3c9
Louis Henry Nayegon <louis.henry.nayegon@gmail.com>
2018-03-03 17:56:48 +0000

    Allow configuartion for in memory or postgres db

---
+++ PackIt/src/PackIt/DbInterface/DbConnectionManager.cs
@@ -0,0 +1,42 @@
+﻿// <copyright company="Simply Code Ltd.">
+// Copyright (c) Simply Code Ltd. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace PackIt.DbInterface
+{
+    using System.Collections.Generic;
+
+    /// <summary> The database connection manager. </summary>
+    public class DbConnectionManager
+    {
+        /// <summary> The registered connections. </summary>
+        private Dictionary<string, IDbContextBuilder> connections;
+
+        /// <summary> Initialises a new instance of the <see cref="DbConnectionManager"/> class. </summary>
+        public DbConnectionManager()
+        {
+            this.connections = new Dictionary<string, IDbContextBuilder>();
+        }
+
+        /// <summary> Registers a context builder. </summary>
+        ///
+        /// <param name="key"> The key. </param>
+        /// <param name="connection"> The connection. </param>
+        public void RegisterContextBuilder(string key, IDbContextBuilder connection)
+        {
+            this.connections[key] = connection;
+        }
+
+        /// <summary> Get a connection. </summary>
+        ///
+        /// <param name="key"> The key to the connection. </param>
+        ///
+        /// <returns> The connection. </returns>
+        public IDbContextBuilder ContextBuilder(string key)
+        {
+            return this.connections[key];
+        }
+    }
+}
---
+++ PackIt/src/PackIt/DbInterface/IDbContextBuilder.cs
@@ -0,0 +1,22 @@
+﻿// <copyright company="Simply Code Ltd.">
+// Copyright (c) Simply Code Ltd. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace PackIt.DbInterface
+{
+    using System;
+    using Microsoft.EntityFrameworkCore;
+
+    /// <summary> Interface for a database connection. </summary>
+    public interface IDbContextBuilder
+    {
+        /// <summary> Creates the context options builder. </summary>
+        ///
+        /// <param name="connection"> The arguments to create the connection. </param>
+        ///
+        /// <returns> The context options builder. </returns>
+        Action<DbContextOptionsBuilder> CreateContextOptionsBuilder(string connection);
+    }
+}
