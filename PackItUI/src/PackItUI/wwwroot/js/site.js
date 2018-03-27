fb5c0b3218552fc6c2de32925f8b99dddae7613e
Louis Henry Nayegon <louis.henry.nayegon@gmail.com>
2018-03-27 17:29:04 +0100

    Add button to delete a row of data from a table

--- PackItUI/src/PackItUI/wwwroot/js/site.js
+++ PackItUI/src/PackItUI/wwwroot/js/site.js
@@ -1,1 +1,25 @@
-﻿// Write your JavaScript code.
+﻿// Copyright (c) Simply Code Ltd. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE file in the project root for full license information.
+
+// find the closest parent node with a matching node name
+//
+// @param e - the node
+// @param name - the parent node name to search for
+function closest(e, name) {
+    var parent = e.parentNode;
+    while (parent != null) {
+        if (parent.nodeName === name) {
+            return parent;
+        }
+        parent = parent.parentNode;
+    }
+    return parent;
+}
+
+// delete a row from a table
+//
+// @param e - the button to delete
+function deleteRow(e) {
+    closest(e, "TABLE").deleteRow(closest(e, "TR").rowIndex);
+}
