2e436deb7846d959dfbcacc3742434f8f39c2c35
Louis Henry Nayegon <louis.henry.nayegon@gmail.com>
2018-05-18 21:41:59 +0100

    Remove JavaScript for packitlib and use yarn instead of bower

--- PackItUI/src/wwwroot/js/packit.js
+++
@@ -1,8 +0,0 @@
-/**
- * @copyright Simply Code Ltd. All rights reserved.
- * @license Licensed under the MIT License.
- * See LICENSE file in the project root for full license information.
- */
-'use strict'
-
-var PackIt = {}
