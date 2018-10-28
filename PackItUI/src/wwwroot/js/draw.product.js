c61abad37f1a5863c3ea3d839486f77582a57ec7
Louis Henry Nayegon <louis.henry.nayegon@gmail.com>
2018-10-28 19:10:26 +0000

    draw a box on each row

--- PackItUI/src/wwwroot/js/draw.product.js
+++ PackItUI/src/wwwroot/js/draw.product.js
@@ -6,20 +6,22 @@
 'use strict'
 
 /* global BABYLON */
-/* global PackIt */
 
-/**
- * Populate the result as a product in the secen
- *
- * @param {BABYLON.Scene} scene - The Babylon scene
- * @param {JSON} result - The result in JSON
- *
- * @returns {BABYLON.Mesh[]} - A list of meshes
- */
+var PackIt;
+(function (PackIt) {
+  /**
+   * Populate the result as a product in the secen
+   *
+   * @param {BABYLON.Scene} scene - The Babylon scene
+   * @param {JSON} result - The result in JSON
+   *
+   * @returns {BABYLON.Mesh[]} - A list of meshes
+   */
+  var drawProduct = function (scene, result) {
+    var meshes = []
 
-PackIt.drawProduct = function (scene, result) {
-  var meshes = []
-
-  meshes.push(new BABYLON.MeshBuilder.CreateBox('box', { width: result.ExternalLength, height: result.ExternalHeight, depth: result.ExternalBreadth }, scene))
-  return meshes
-}
+    meshes.push(new BABYLON.MeshBuilder.CreateBox('box', { width: result.ExternalLength, height: result.ExternalHeight, depth: result.ExternalBreadth }, scene))
+    return meshes
+  }
+  PackIt.drawProduct = drawProduct
+})(PackIt || (PackIt = {}))
