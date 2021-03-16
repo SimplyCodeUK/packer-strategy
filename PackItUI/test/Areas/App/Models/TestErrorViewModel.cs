e2b7ce169d14560edab24a216edafefc051fc3dd
Louis Henry Nayegon <louis.henry.nayegon@gmail.com>
2021-03-16 16:48:26 +0000

    exclude cshtml from code coverage

---
+++ PackItUI/test/Areas/App/Models/TestErrorViewModel.cs
@@ -0,0 +1,41 @@
+// <copyright company="Simply Code Ltd.">
+// Copyright (c) Simply Code Ltd. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace PackItUI.Test.Areas.App.Controllers
+{
+    using NUnit.Framework;
+    using PackItUI.Areas.App.Models;
+
+    /// <summary> (Unit Test Fixture) a controller for handling test materials. </summary>
+    [TestFixture]
+    public class TestErrorViewModel
+    {
+
+        /// <summary> (Unit Test Method) index action. </summary>
+        [Test]
+        public void CanShowRequestId()
+        {
+            ErrorViewModel model = new ErrorViewModel();
+
+            model.RequestId = "";
+
+            Assert.AreEqual("", model.RequestId);
+            Assert.False(model.ShowRequestId);
+        }
+
+        /// <summary> (Unit Test Method) about action when the services are not running. </summary>
+        [Test]
+        public void CannotShowRequestId()
+        {
+            ErrorViewModel model = new ErrorViewModel();
+
+            model.RequestId = "id";
+
+            Assert.AreEqual("id", model.RequestId);
+            Assert.True(model.ShowRequestId);
+        }
+    }
+}
